e9a3ce86068b0a88c3e21f4923b369cf40cf3b8c
Adrian-Marin <a18310176@live.ceti.mx>
2021-10-14 22:31:39 -0500

    App Screenshot JAVA
    Java Application that takes a complete screenshot

---
+++ Java/Recorder/src/recorder/Recorder.java
@@ -0,0 +1,16 @@
+package recorder;
+
+/**
+ *
+ * @author Adrian
+ */
+public class Recorder {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        App app = new App();
+    }
+    
+}
---
+++ Recorder/Recorder/Controller.cs
@@ -0,0 +1,22 @@
+﻿using System.Graphics.Canvas;
+using System.Graphics.Canvas.UI.Composition;
+using System;
+using System.Numerics;
+using System.Threading.Tasks;
+using System.Foundation;
+using System.Graphics;
+using System.Graphics.Capture;
+using System.Graphics.DirectX;
+using System.Storage;
+using System.UI;
+using System.UI.Composition;
+using System.UI.Xaml;
+using System.UI.Xaml.Controls;
+using System.UI.Xaml.Hosting;
+
+namespace Recorder
+{
+    class Controller
+    {
+    }
+}
